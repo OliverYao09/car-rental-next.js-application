7caf865e8e204c2b2b5382885d86ff3fb383690a
OliverYao09 <53160804+OliverYao09@users.noreply.github.com>
2023-08-12 00:38:02 +1200

    feature: set up the project config

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero_title"></h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
