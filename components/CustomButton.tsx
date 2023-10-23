830c876cc90f53d852f67c0317427f1e4a754ac9
OliverYao09 <53160804+OliverYao09@users.noreply.github.com>
2023-10-23 15:38:17 +1300

    feature: create a car cataloguesection that display cars available

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,10 +1,15 @@
 import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
+import { text } from 'stream/consumers';
 
 const CustomButton = ({
   title,
   containerStyles,
   handleClick,
   btnType,
+  rightIcon,
+  textStyles,
+  isDisabled,
 }: CustomButtonProps) => {
   return (
     <button
@@ -13,7 +18,17 @@ const CustomButton = ({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right-icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
